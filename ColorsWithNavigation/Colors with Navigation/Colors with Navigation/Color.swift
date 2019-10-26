023497c35e3903c7285820c3bb4cc9812d37b297
ChaseMiles <42549771+ChaseMiles@users.noreply.github.com>
2019-10-25 19:19:05 -0500

    Add files via upload

---
+++ ColorsWithNavigation/Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Chase  Miles on 10/25/19.
+//  Copyright © 2019 Chase  Miles. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
